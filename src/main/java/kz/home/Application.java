12eb892d3e87b1d91cb5664e6a9b2f45efe70a01
Daniyar Kalmurzin <daniya.kalmurzin@gmail.com>
2017-05-05 17:19:11 +0600

    application added logger

--- src/main/java/kz/home/Application.java
+++ src/main/java/kz/home/Application.java
@@ -2,6 +2,8 @@ package kz.home;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,18 +12,20 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-    
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
-    	SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
-    
-    public CommandLineRunner commandLineRunnable(ApplicationContext ctx){
+
+    @Bean
+    public CommandLineRunner commandLineRunnable(ApplicationContext ctx) {
         return args -> {
-            System.out.println("lets inspect the beans provided by spring boot");
+            logger.info("lets inspect the beans provided by spring boot");
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
-            for (String beanName: beanNames){
-                System.out.println(beanName);
+            for (String beanName : beanNames) {
+                logger.info(beanName);
             }
         };
     }
