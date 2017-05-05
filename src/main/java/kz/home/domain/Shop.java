c4779a140e138ad7a0b7789c07d6c66748ab894f
Daniyar Kalmurzin <daniya.kalmurzin@gmail.com>
2017-05-05 17:19:39 +0600

    new controller HelloWorld with test

---
+++ src/main/java/kz/home/domain/Shop.java
@@ -0,0 +1,17 @@
+package kz.home.domain;
+
+/**
+ * Shop
+ * Created by daniyar.kalmurzin@gmail.com on 5/5/17.
+ */
+public class Shop {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
